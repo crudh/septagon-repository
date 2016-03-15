f0cbd451e8ef6673acf94a63aebb5a2bae604008
Christian Rudh <christian@rudh.se>
2016-03-15 22:35:56 +0100

    Cleanups

--- server/server_.js
+++ server/server_.js
@@ -4,20 +4,22 @@ import express from 'express';
 import metadata from './metadata_web';
 import proxy from './proxy_web';
 
-const environment = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.set('env', environment);
+app.set('env', env);
 
 metadata.init(app);
 proxy.init(app);
 
-app.get('*', req => {
+app.use((req, res) => {
   console.log('*** catch all:', req.url);
+  res.status(404).send({ message: 'Not found' });
 });
 
+// This may conflict with the proxying using request with POST, there are several solutions
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,5 +29,5 @@ app.listen(port, host, err => {
     return;
   }
 
-  console.log(`Server running in ${environment} mode (http://${host}:${port})`);
+  console.log(`Server running in ${env} mode (http://${host}:${port})`);
 });
