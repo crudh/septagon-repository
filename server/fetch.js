a3c14ede2b5fc8a3b0c3ce286ef65a1fc1751189
Christian Rudh <christian@rudh.se>
2016-03-18 14:52:58 +0100

    Wrapped request in a promise so we can reuse it for the other fetch operations

--- server/fetch.js
+++ server/fetch.js
@@ -3,19 +3,22 @@ import config from './config';
 
 // TODO fix error returns
 
+const requestPromise = options => new Promise((resolve, reject) => {
+  request(options, (error, response, body) => {
+    if (error) {
+      reject(error);
+    } else if (response.statusCode < 200 || response.statusCode >= 300) {
+      reject(new Error('statusCode was not 2XX'));
+    } else {
+      resolve({ body, response });
+    }
+  });
+});
+
 export const fetchPackage = name => new Promise((resolve, reject) => {
   console.log(`* ${name} - fetching package from upstream`);
 
-  request(`${config.upstream}/${name}`, (error, response, body) => {
-    if (error) {
-      console.log(`* ${name} - fetch - failed to fetch package from upsteam`);
-      reject(error);
-    } else if (response.statusCode < 200 || response.statusCode >= 300) {
-      console.log(`* ${name} - fetch - failed to fetch package from upsteam`);
-      reject(new Error('statusCode was not 2XX'));
-    } else {
-      console.log(`* ${name} - fetch - successfully fetched package from upsteam`);
-      resolve(body);
-    }
-  });
+  requestPromise(`${config.upstream}/${name}`)
+    .then(data => resolve(data.body))
+    .catch(err => reject(err));
 });
