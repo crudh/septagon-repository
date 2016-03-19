0200b75bbc90c0990d79fc760145f8d430070a5e
Christian Rudh <christian@rudh.se>
2016-03-19 23:17:13 +0100

    Cleanups

--- server/package.js
+++ server/package.js
@@ -4,15 +4,15 @@ import config from './config';
 
 const getFileName = (name, version) => version ? `${name}-${version}` : name;
 
-const statPackageDir = name => new Promise((resolve, reject) => {
-  console.log(`* ${name} - store - checking if package dir exists`);
-  fs.stat(`${config.storage}/${name}`, err => err ? reject(err) : resolve());
-});
+const getUpstreamUrl = (name, version) => {
+  const baseUrl = `${config.upstream}/${name}`;
+  if (!version) return baseUrl;
 
-const statPackageFile = (name, version) => new Promise((resolve, reject) => {
-  const fileName = getFileName(name, version);
-  console.log(`* ${name} - store - checking if package file exists`);
-  fs.stat(`${config.storage}/${name}/${fileName}`, err => err ? reject(err) : resolve());
+  return `${baseUrl}/${version}`;
+};
+
+const statPath = path => new Promise((resolve, reject) => {
+  fs.stat(path, err => err ? reject(err) : resolve());
 });
 
 const createPackageDir = name => new Promise((resolve, reject) => {
@@ -21,42 +21,38 @@ const createPackageDir = name => new Promise((resolve, reject) => {
 });
 
 export const fetchPackage = (name, version) => new Promise((resolve, reject) => {
+  const packageDir = `${config.storage}/${name}`;
   const fileName = getFileName(name, version);
+  const packageFilePath = `${packageDir}/${fileName}`;
   console.log(`* ${name} - store - fetching package`);
 
-  statPackageDir(name)
+  statPath(packageDir)
     .catch(() => createPackageDir(name))
     .catch(err => {
       console.log(`* ${name} - store - failed to create package directory in store: ${err}`);
       reject(err);
     })
-    .then(() => statPackageFile(name, version))
+    .then(() => statPath(packageFilePath))
     .then(() => {
-      const readStream = fs.createReadStream(`${config.storage}/${name}/${fileName}`);
+      const readStream = fs.createReadStream(packageFilePath);
       resolve(readStream);
     })
     .catch(() => {
       console.log(`* ${name} - store - package not available in store`);
 
-      let url = `${config.upstream}/${name}`;
-      if (version) {
-        url += `/${version}`;
-      }
-
-      request(url)
+      request(getUpstreamUrl(name, version))
         .on('error', err => {
           console.log(`* ${name} - store - network error when fetching package from upstream: ${err}`);
           reject(err);
         })
         .pipe(
-          fs.createWriteStream(`${config.storage}/${name}/${fileName}`)
+          fs.createWriteStream(packageFilePath)
             .on('error', err => {
               console.log(`* ${name} - store - error when saving package to store: ${err}`);
               reject(err);
             })
             .on('finish', () => {
-              const readStream = fs.createReadStream(`${config.storage}/${name}/${fileName}`);
-              resolve(readStream);
+              resolve(fs.createReadStream(packageFilePath));
             })
         );
     });
