ff6fab68bcb6f3f9d6d349155e0f634aa167ce51
Christian Rudh <christian@rudh.se>
2016-03-19 22:44:41 +0100

    Moved everything from package to package_web

--- server/api/package/package_web.js
+++ server/api/package/package_web.js
@@ -1,4 +1,16 @@
-import { getPackageHandler } from './package';
+import { fetchPackage } from '../../store';
+
+const getPackageHandler = (req, res, name, version) => {
+  fetchPackage(name, version)
+    .then(readStream => {
+      readStream.pipe(res);
+    })
+    .catch(err => {
+      const statusCode = (err && err.statusCode) || 500;
+      console.log(`* ${name} - package_web - got an error when getting the package from the store: ${err}`);
+      res.status(statusCode).send({ message: 'internal error' });
+    });
+};
 
 export const init = app => {
   app.get('/registry/main/:name', (req, res) => {
