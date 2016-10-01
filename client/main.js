d5c6e1335e43593e02c7660cd397b86de835a068
Christian Rudh <christian@rudh.se>
2016-10-01 21:01:36 +0200

    Initial stuff for the client build

---
+++ client/main.js
@@ -0,0 +1,56 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+import routes from './routes/routes';
+import rootReducer from './reducers/reducers';
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const middlewares = [
+  thunk,
+  promiseMiddleware({
+    promiseTypeSuffixes: ['REQUEST', 'SUCCESS', 'FAILURE']
+  })
+];
+
+injectTapEventPlugin();
+
+if (nodeEnv === 'development') {
+  middlewares.push(createLogger());
+}
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares)
+);
+
+if (nodeEnv === 'development') {
+  console.log('[Development mode activated]');
+}
+
+const history = syncHistoryWithStore(browserHistory, store);
+const rootElement = document.getElementById('root');
+
+try {
+  render(
+    <Provider store={store}>
+      <Router children={routes} history={history}/>
+    </Provider>,
+    rootElement
+  );
+} catch (error) {
+  if (nodeEnv === 'development') {
+    throw error;
+  } else {
+    // TODO better production handling of errors later
+    throw error;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const config = {
+  entry: path.join(__dirname, 'client/main.js'),
+  output: {
+    path: path.join(__dirname, 'public/build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(nodeEnv)
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?cacheDirectory'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
+
+if (nodeEnv === 'production') {
+  config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+if (nodeEnv === 'development') {
+  config.devtool = 'cheap-module-source-map';
+
+  config.plugins.push(
+    new webpack.NoErrorsPlugin()
+  );
+}
+
+module.exports = config;
