4ddb12ee7bbad968dd5efb4b0c2503013aa5fb7f
Christian Rudh <christian@rudh.se>
2017-01-15 17:14:04 +0100

    Fixed so everything else than /npm/* is handled by the client

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,4 @@
+const logger = require('winston');
 const { createValidation, validatorRepository } = require('./api/common_api');
 
 const validate = createValidation(validatorRepository);
@@ -9,6 +10,11 @@ const routes = (app, api) => {
   app.get('/npm/:repo/:name/:version?', validate(api.package.fetchPackage));
   app.get('/npm/:repo/:name/-/:distFile', validate(api.package.fetchDistFile));
   app.get('/npm/:repo/-/all*', validate(api.package.searchPackage));
+
+  app.get('/npm/*', (req, res) => {
+    logger.error(`Unhandled URL: ${req.url}`);
+    res.status(404).send({ message: 'Not found' });
+  });
 };
 
 module.exports = routes;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const config = require('config');
 const express = require('express');
 const mkdirp = require('mkdirp');
 const logger = require('winston');
+const path = require('path');
 const routes = require('./routes');
 const packagesApi = require('./api/packages_api');
 const registryApi = require('./api/registry_api');
@@ -42,12 +43,10 @@ routes(app, {
   registry: registryApi
 });
 
-app.use((req, res) => {
-  logger.error(`Unhandled URL: ${req.url}`);
-  res.status(404).send({ message: 'Not found' });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
-// This may conflict with the proxying using request with POST, there are several solutions
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
