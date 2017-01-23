d26011afc578937f4594490e3b8c1648f711c664
Christian Rudh <christian@rudh.se>
2017-01-23 20:51:22 +0100

    Added a test request on the index page

--- client/containers/Index.js
+++ client/containers/Index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import request from '../utils/request';
 
-const Index = () => (
-  <div>
-    Index
-  </div>
-);
+class Index extends Component {
+  componentDidMount() {
+    request('/admin/config/repos').then(
+      ({ json }) => console.info('success', json),
+      error => console.error('fail', error)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Index
+      </div>
+    );
+  }
+}
 
 export default Index;
