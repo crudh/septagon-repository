8d0c3b95fc6bc3ab2f7cc0d179144487a0605d04
Christian Rudh <christian@rudh.se>
2017-05-02 09:17:24 +0200

    Always get the main package file from upstream, if available. Fallback to local cache

--- server/npm/handlers/package.js
+++ server/npm/handlers/package.js
@@ -49,44 +49,48 @@ const getMainPackage = (repo, name, callback) => {
   const directoryPath = `${repo.storage}/${name}`;
   const filePath = `${directoryPath}/${fileName}`;
 
-  return checkPackageFile(repo, name, null, errFile => {
-    if (!errFile) return streamPackage(repo, filePath, callback);
-    if (!repo.upstream) return callback({ statusCode: 404 });
+  if (!repo.upstream) {
+    return checkPackageFile(repo, name, null, errFile => {
+      return errFile ? callback({ statusCode: 404 }) : streamPackage(repo, filePath, callback);
+    });
+  }
 
-    const req = request(getUpstreamUrl(repo, name));
-    req.pause();
+  const req = request(getUpstreamUrl(repo, name));
+  req.pause();
 
-    return req
-      .on("error", errRequest => {
-        logger.error(`Network error when fetching package ${name} from upstream`);
-        callback(errRequest);
-      })
-      .on("response", response => {
-        const statusCode = response.statusCode;
+  return req
+    .on("error", errRequest => {
+      logger.error(`Network error when fetching package ${name} from upstream, checking local cache`);
 
-        if (statusCode < 200 || statusCode >= 300) {
-          const error = new Error(`Got ${statusCode} when fetching package from upstream`);
-          error.statusCode = statusCode;
-          return callback(error);
-        }
-
-        return mkdirp(directoryPath, errDir => {
-          if (errDir) return callback(errDir);
-
-          req.pipe(
-            fs
-              .createWriteStream(filePath)
-              .on("error", errWrite => {
-                logger.error(`Error when writing package ${name} to storage`);
-                callback(errWrite);
-              })
-              .on("finish", () => streamPackage(repo, filePath, callback))
-          );
-
-          return req.resume();
-        });
+      return checkPackageFile(repo, name, null, errFile => {
+        return errFile ? callback(errRequest) : streamPackage(repo, filePath, callback);
       });
-  });
+    })
+    .on("response", response => {
+      const statusCode = response.statusCode;
+
+      if (statusCode < 200 || statusCode >= 300) {
+        const error = new Error(`Got ${statusCode} when fetching package from upstream`);
+        error.statusCode = statusCode;
+        return callback(error);
+      }
+
+      return mkdirp(directoryPath, errDir => {
+        if (errDir) return callback(errDir);
+
+        req.pipe(
+          fs
+            .createWriteStream(filePath)
+            .on("error", errWrite => {
+              logger.error(`Error when writing package ${name} to storage`);
+              callback(errWrite);
+            })
+            .on("finish", () => streamPackage(repo, filePath, callback))
+        );
+
+        return req.resume();
+      });
+    });
 };
 
 const getVersionedPackage = (repo, name, version, callback) => {
