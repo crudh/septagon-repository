63c5d6a46f52a412e1bf39a72c91ea227a0adf9e
Christian Rudh <christian@rudh.se>
2017-05-17 09:32:20 +0200

    Added initial tests for config validation

---
+++ server/test/utils/validate_config.test.js
@@ -0,0 +1,16 @@
+const config = require("config");
+const { validateServerConfig } = require("../../utils/validate_config");
+
+const serverConfig = config.get("server");
+
+describe("Validate config", () => {
+  describe("Server config", () => {
+    it("should validate an empty config with all errors", () => {
+      expect(validateServerConfig({}).length).toEqual(6);
+    });
+
+    it("should validate a correct file without errors", () => {
+      expect(validateServerConfig(serverConfig).length).toEqual(0);
+    });
+  });
+});
