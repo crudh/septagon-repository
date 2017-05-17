c4dac2fc4e6c567c9514e2e16b2a81dbed05b97e
Christian Rudh <christian@rudh.se>
2017-05-17 09:32:02 +0200

    Added initial code for config validation

---
+++ server/utils/validate_config.js
@@ -0,0 +1,32 @@
+const _get = require("lodash/fp/get");
+
+const isSet = () => (key, config) => (_get(key, config) ? "" : `${key}: should be set`);
+
+const isAny = (...options) => (key, config) =>
+  (options.includes(_get(key, config)) ? "" : `${key}: should be any of [${options}]`);
+
+const runChecks = (state, config, ...checks) =>
+  checks.reduce((checksState, check) => {
+    const [key, ...ops] = check;
+    return [...checksState, ...ops.map(op => op(key, config)).filter(_ => _)];
+  }, state);
+
+const validateLocation = (state, config) =>
+  runChecks(
+    state,
+    config,
+    ["location", isSet()],
+    ["location.protocol", isSet(), isAny("http", "https")],
+    ["location.host", isSet()],
+    ["location.port", isSet()]
+  );
+
+const validateRepos = (state, config) => runChecks(state, config, ["repos", isSet()]);
+
+const validateServerConfig = config => {
+  return [validateLocation, validateRepos].reduce((state, validationFunc) => validationFunc(state, config), []);
+};
+
+module.exports = {
+  validateServerConfig
+};
