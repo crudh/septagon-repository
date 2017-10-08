c7c2a15b5231c858acb81edcba4d0d1d2c553001
Christian Rudh <christian@rudh.se>
2017-10-08 16:00:03 +0200

    Pass value to checks and rename variables

--- server/utils/validateConfig.js
+++ server/utils/validateConfig.js
@@ -1,41 +1,35 @@
 const _flatten = require("lodash/fp/flatten");
 const _flow = require("lodash/fp/flow");
-const _get = require("lodash/fp/get");
 const _getOr = require("lodash/fp/getOr");
 const _isFinite = require("lodash/fp/isFinite");
 const _isPlainObject = require("lodash/fp/isPlainObject");
 const _keys = require("lodash/fp/keys");
 const _map = require("lodash/fp/map");
 
-const isSet = () => (key, config) =>
-  _get(key, config) ? "" : `${key}: should be set`;
+const isSet = () => (key, value) => (value ? "" : `${key}: should be set`);
 
-const isAny = (...options) => (key, config) =>
-  options.includes(_get(key, config))
-    ? ""
-    : `${key}: should be any of [${options}]`;
+const isAny = (...options) => (key, value) =>
+  options.includes(value) ? "" : `${key}: should be any of [${options}]`;
 
-const isEqual = value => (key, config) =>
-  _get(key, config) === value ? "" : `${key}: should match ${value}`;
+const isEqual = otherValue => (key, value) =>
+  value === otherValue ? "" : `${key}: should match ${otherValue}`;
 
-const isObject = () => (key, config) =>
-  _isPlainObject(_get(key, config)) ? "" : `${key}: should be an object`;
+const isObject = () => (key, value) =>
+  _isPlainObject(value) ? "" : `${key}: should be an object`;
 
-const isNumber = () => (key, config) =>
-  _isFinite(parseInt(_get(key, config), 10))
-    ? ""
-    : `${key}: should be a number`;
+const isNumber = () => (key, value) =>
+  _isFinite(parseInt(value, 10)) ? "" : `${key}: should be a number`;
 
-const hasChild = () => (key, config) =>
-  _keys(_get(key, config)).length > 0
-    ? ""
-    : `${key}: should have at least one child`;
+const hasChild = () => (key, value) =>
+  _keys(value).length > 0 ? "" : `${key}: should have at least one child`;
 
 const runChecks = (config, ...checks) =>
   checks.reduce(
-    (checksState, [key, ...ops]) => [
+    (checksState, [key, ...checkFuncs]) => [
       ...checksState,
-      ...ops.map(op => op(key, config)).filter(_ => _)
+      ...checkFuncs
+        .map(checkFunc => checkFunc(key, _getOr(null, key, config), config))
+        .filter(_ => _)
     ],
     []
   );
