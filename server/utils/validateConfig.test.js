05de2d5052250b90e0dcefbb2face4f7d08376d2
Christian Rudh <christian@rudh.se>
2017-10-08 16:00:23 +0200

    Extended tests to check empty repo config

--- server/utils/validateConfig.test.js
+++ server/utils/validateConfig.test.js
@@ -21,10 +21,27 @@ const defaultConfig = {
   }
 };
 
+const emptyRepoConfig = {
+  server: {
+    location: {
+      protocol: "http",
+      host: "localhost",
+      port: "3000"
+    },
+    repos: {
+      main: {}
+    }
+  }
+};
+
 describe("Validate config", () => {
   describe("Server config", () => {
     it("should validate an empty config with all errors", () => {
       expect(validateServerConfig({}).length).toEqual(10);
+    });
+
+    it("should validate an empty repo with all errors", () => {
+      expect(validateServerConfig(emptyRepoConfig.server).length).toEqual(3);
     });
 
     it("should validate a correct file without errors", () => {
